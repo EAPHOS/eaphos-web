40f75ec6b11115e89e95e3d9073290cb069e9fe4
address0 <soyong6624@gmail.com>
2025-07-19 17:52:15 +0900

    feat: R&D 음극재 소재 - KR 구현

--- src/pages/kr/RD/RDAnodeMaterial.tsx
+++ src/pages/kr/RD/RDAnodeMaterial.tsx
@@ -4,7 +4,7 @@ const RDAnodeMaterial = () => {
   return (
     <div>
       <PageNav
-        title='음극재소재'
+        title='음극재 소재'
         parentTitle='R&D'
         parentPath='/kr/rd/anode-material'
         siblings={[
@@ -16,8 +16,64 @@ const RDAnodeMaterial = () => {
         ]}
         imageClass='rndTop'
       />
-      <h1>양극재 R&D</h1>
-      <p>양극재 연구개발 페이지입니다.</p>
+      <div className='s-comm container'>
+        <h3>연구 배경 및 필요성</h3>
+        <div className='pdb20'></div>
+      </div>
+      <section className='container com-comm s-comm pro01'>
+        <div className='pdt40'></div>
+        <ul className='twoDiv'>
+          <li>
+            <h4>PEDOT : PSS</h4>
+            <h5>
+              - For CNT
+              <br />- From Si
+            </h5>
+          </li>
+          <div className='pdb20'></div>
+          <p>
+            <img
+              src='/src/assets/pro00-01.jpg'
+              className='res-img'
+              alt='anode-material'
+            />
+          </p>
+        </ul>
+        <ul className='pro-ul'>
+          <li>
+            <h4>
+              Conductivity comparison between conductive polymer and other
+              materials
+            </h4>
+            <p>
+              <img
+                src='/src/assets/pro00-02.png'
+                className='res-img'
+                alt='conductivity-comparison'
+              />
+            </p>
+          </li>
+          <li>
+            <h4>The Shape of PEDOT:PSS </h4>
+            <h5>
+              Shell(PSS rich region) is composed with PSS having sulfonyl group,
+              as being similar to cation ion exchange resin.
+            </h5>
+            <p className='center'>
+              <img src='/src/assets/pro00-03.png' alt='PSS' />
+            </p>
+          </li>
+        </ul>
+        <div className='pdb50'></div>
+        <p className='center'>
+          <img
+            src='/src/assets/pro00-04.png'
+            className='res-img'
+            alt='faster-charging'
+          />
+        </p>
+      </section>
+      <section className='rnd05-01 container s-pad-bt com-comm s-comm'></section>
     </div>
   );
 };
